85122928024faa0036d58031875192f8465405b4
Denis Beloshitsky <denis.beloshitsky@vk.team>
2023-09-16 14:36:25 +0300

    [Dev]: Initial commit

---
+++ Movie Choosing Companion/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Movie Choosing Companion
+//
+//  Created by denis.beloshitsky on 11.09.2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession,
+                     options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {}
+}
---
+++ Movie Choosing Companion/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  Movie Choosing Companion
+//
+//  Created by denis.beloshitsky on 11.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
