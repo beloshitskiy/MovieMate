c23379af5826b7b6a606b47424f5bda3e71d57b4
Denis Beloshitskiy <beloshitskiy.denis@gmail.com>
2023-12-09 21:43:24 +0300

    IOS-9: Create genre selection screen

--- MovieMate/GenrePage/GenrePageViewController.swift
+++ MovieMate/GenrePage/GenrePageViewController.swift
@@ -5,26 +5,28 @@
 //  Created by denis.beloshitsky on 30.10.2023.
 //
 
+import SnapKit
 import UIKit
 
 final class GenrePageViewController: UIViewController {
-    
-}
+    private let genreView: GenrePageView
+    private let genresPageVM: GenrePageViewModel
 
-extension GenrePageViewController: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        <#code#>
+    init() {
+        genresPageVM = GenrePageViewModel()
+        genreView = GenrePageView(viewModel: genresPageVM)
+        super.init(nibName: nil, bundle: nil)
     }
-}
 
-extension GenrePageViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.addSubview(genreView)
+        genreView.snp.makeConstraints { $0.edges.equalToSuperview() }
     }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
-    }
-    
 
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
--- MovieMate/GenrePage/GenrePageViewModel.swift
+++ MovieMate/GenrePage/GenrePageViewModel.swift
@@ -5,24 +5,60 @@
 //  Created by denis.beloshitsky on 30.10.2023.
 //
 
+import Combine
 import Foundation
 import UIKit
 
-final class GenrePageViewModel {
-    let genres: [Genre]
+final class GenrePageViewModel: NSObject {
+    private var genres: [Genre] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map { Genre(title: "\($0)") }
+    @Published private(set) var selectedPaths: [IndexPath] = []
 
-    private var chosenGenres: Set<Genre> = []
+//    override init() {
+//        super.init()
+//    }
 
-    weak var vc: (UIViewController & UITableViewDelegate & UITableViewDataSource)?
-
-    private let apiClient = ApiClient.shared
-
-    init() {
-        genres = apiClient.getGenres()
+    func chooseGenreRandomly() {
+        guard let random = genres.randomElement() else { return }
+        ApiClient.shared.confirmSelected([random])
     }
 
-    func confirmChosenGenres() {
-        apiClient.confirmChosen(genres: chosenGenres)
+    func confirmSelected(_ tableView: UITableView) {
+        let genres: [Genre] = selectedPaths.compactMap {
+            (tableView.cellForRow(at: $0) as? GenreCell)?.genre
+        }
+
+        ApiClient.shared.confirmSelected(genres)
+    }
+}
+
+extension GenrePageViewModel: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        guard let cell = tableView.cellForRow(at: indexPath) as? GenreCell else { return }
+
+        if !cell.isChosen {
+            selectedPaths.append(indexPath)
+        } else {
+            selectedPaths.removeAll { $0 == indexPath }
+        }
+
+        cell.isChosen.toggle()
+    }
+}
+
+extension GenrePageViewModel: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        1
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        genres.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let genre = genres[safe: indexPath.row] else { return UITableViewCell() }
+        let cell = tableView.dequeueReusableCell(withClass: GenreCell.self, for: indexPath)
+        cell.configure(with: genre)
+        return cell
+    }
 }
