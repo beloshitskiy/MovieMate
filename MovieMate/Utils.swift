ff4cfc2c03bdb0b514469ee990dd7bbac712cab8
Denis Beloshitskiy <beloshitskiy.denis@gmail.com>
2023-12-09 21:39:36 +0300

    [Dev] project clean-up

--- MovieMate/Utils.swift
+++ MovieMate/Utils.swift
@@ -25,3 +25,45 @@ extension UIView {
         roundCorners(radius: bounds.height / 2.0)
     }
 }
+
+extension UIStackView {
+    func addArrangedSubviews(_ views: [UIView]) {
+        views.forEach { self.addArrangedSubview($0) }
+    }
+}
+
+extension Collection {
+    subscript(safe index: Index) -> Element? {
+        return indices.contains(index) ? self[index] : nil
+    }
+}
+
+extension UIColor {
+    convenience init(hex: UInt) {
+        self.init(red: CGFloat((hex & 0xFF0000) >> 16) / 255.0,
+                  green: CGFloat((hex & 0x00FF00) >> 8) / 255.0,
+                  blue: CGFloat(hex & 0x0000FF) / 255.0,
+                  alpha: CGFloat(1.0))
+    }
+}
+
+extension UIImage {
+    func resize(targetSize: CGSize) -> UIImage {
+        return UIGraphicsImageRenderer(size:targetSize).image { _ in
+            self.draw(in: CGRect(origin: .zero, size: targetSize))
+        }
+    }
+}
+
+extension UITableView {
+    func register<T: UITableViewCell>(cellWithClass name: T.Type) {
+        self.register(name, forCellReuseIdentifier: String(describing: name))
+    }
+
+    func dequeueReusableCell<T: UITableViewCell>(withClass name: T.Type, for indexPath: IndexPath) -> T {
+        guard let cell = self.dequeueReusableCell(withIdentifier: String(describing: name), for: indexPath) as? T else {
+            fatalError("Couldn't find UITableViewCell for \(String(describing: name))")
+        }
+        return cell
+    }
+}
