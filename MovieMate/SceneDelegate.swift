200919b00cdf4166e2c74d6ef84c2ad694eba491
Denis Beloshitskiy <beloshitskiy.denis@gmail.com>
2023-12-15 01:00:52 +0300

    [Dev] Update starting point

--- MovieMate/SceneDelegate.swift
+++ MovieMate/SceneDelegate.swift
@@ -13,7 +13,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = WelcomePageViewController()
+        let nvc = UINavigationController(rootViewController: WelcomePageViewController())
+        nvc.navigationBar.tintColor = .white
+        window.rootViewController = nvc
         window.makeKeyAndVisible()
         self.window = window
     }
