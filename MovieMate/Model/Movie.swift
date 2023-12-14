c1f5ce7676386fb6f7282c15c3ff9b0c92b716e4
Denis Beloshitskiy <beloshitskiy.denis@gmail.com>
2023-12-15 00:53:19 +0300

    [Dev] Update models

--- MovieMate/Model/Movie.swift
+++ MovieMate/Model/Movie.swift
@@ -7,24 +7,15 @@
 
 import Foundation
 
-struct Rating: Decodable {
-    let provider: String
-    let rating: String
-}
-
-struct Genre: Hashable, Decodable {
-    let name: String
-}
-
 struct Movie {
-    let id: UUID // or str
+    let id: String
     let name: String
     let description: String
     let posterURL: URL?
     let releaseYear: Int
     let duration: String
-    let genres: [Genre]
-    let ratings: [Rating]
+    let genres: [String]
+    let rating: String
 }
 
 extension Movie: Decodable {
@@ -36,12 +27,12 @@ extension Movie: Decodable {
         case releaseYear = "release_year"
         case duration
         case genres
-        case ratings
+        case rating
     }
 
     init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
-        id = try container.decode(UUID.self, forKey: .id)
+        id = try container.decode(String.self, forKey: .id)
         name = try container.decode(String.self, forKey: .name)
         description = try container.decode(String.self, forKey: .description)
         posterURL = URL(string: try container.decode(String.self, forKey: .posterURL))
@@ -54,7 +45,18 @@ extension Movie: Decodable {
         formatter.allowedUnits = [.hour, .minute]
         duration = formatter.string(from: durationInMinutes) ?? ""
 
-        genres = try container.decode([Genre].self, forKey: .genres)
-        ratings = try container.decode([Rating].self, forKey: .ratings)
+        genres = try container.decode([String].self, forKey: .genres)
+        rating = try container.decode(String.self, forKey: .rating)
     }
+}
+
+extension Movie {
+    static let mock = Movie(id: "",
+                            name: "Драйв",
+                            description: "Великолепный водитель – при свете дня он выполняет каскадерские трюки на съёмочных площадках Голливуда, а по ночам ведет рискованную игру. Но один опасный контракт – и за его жизнь назначена награда. Теперь, чтобы остаться в живых и спасти свою очаровательную соседку, он должен делать то, что умеет лучше всего – виртуозно уходить от погони.",
+                            posterURL: URL(string: "https://avatars.mds.yandex.net/get-kinopoisk-image/1773646/921089d1-4ebd-4b6f-be74-bfe29b21fad1/orig")!,
+                            releaseYear: 2011,
+                            duration: "1ч 40 мин",
+                            genres: ["криминал", "драма", "триллер"],
+                            rating: "7.8")
 }
