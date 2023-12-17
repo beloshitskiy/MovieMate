34a5ab65630a715d2a3ce4111de8218e41a43d11
Denis Beloshitskiy <beloshitskiy.denis@gmail.com>
2023-12-17 22:21:02 +0300

    IOS-13: Update DeckPage’s design

--- MovieMate/DeckPage/DeckPageDataSource.swift
+++ MovieMate/DeckPage/DeckPageDataSource.swift
@@ -43,12 +43,24 @@ final class DeckPageDataSource {
         MovieCardViewModel.onUndoTap = { [weak self] in
             self?.stack?.undoLastSwipe(animated: true)
         }
+
+        MovieCardViewModel.onDescriptionTap = { [weak self] in
+            let descriptionVC = MovieCardDescriptionViewController()
+            descriptionVC.movieDescription = $0
+
+            if let sheet = descriptionVC.sheetPresentationController {
+                sheet.detents = [.medium(), .large()]
+//                sheet.preferredCornerRadius = 20.0
+            }
+
+            self?.vc?.present(descriptionVC, animated: true)
+        }
     }
 }
 
 extension DeckPageDataSource: SwipeCardStackDataSource {
     func cardStack(_ cardStack: SwipeCardStack, cardForIndexAt index: Int) -> SwipeCard {
-        MovieCardFactory.makeCard(for: movies[safe: index])
+        MovieCardFactory.makeCard(for: movies[safe: index], index: index)
     }
 
     func numberOfCards(in cardStack: SwipeCardStack) -> Int {
@@ -57,10 +69,10 @@ extension DeckPageDataSource: SwipeCardStackDataSource {
 }
 
 enum MovieCardFactory {
-    static func makeCard(for movie: Movie?) -> SwipeCard {
+    static func makeCard(for movie: Movie?, index: Int) -> SwipeCard {
         guard let movie else { return SwipeCard() }
         let view = MovieCardView()
-        let viewModel = MovieCardViewModel(with: movie)
+        let viewModel = MovieCardViewModel(with: movie, canUndo: index != 0)
         view.configure(with: viewModel)
 
         let card = SwipeCard()
