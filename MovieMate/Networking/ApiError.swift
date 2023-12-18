79ab31f5227d8aaa234bbb995dd9ac89e8051e2b
Denis Beloshitskiy <beloshitskiy.denis@gmail.com>
2023-12-18 11:39:33 +0300

    IOS-17: Add force routing after timeout

--- MovieMate/Networking/ApiError.swift
+++ MovieMate/Networking/ApiError.swift
@@ -13,7 +13,7 @@ struct ErrorInfo {
     let buttonTitle: String
     let action: (() -> Void)?
 
-    init(_ title: String, _ message: String, _ buttonTitle: String, _ action: (() -> Void)?) {
+    init(_ title: String, _ message: String, _ buttonTitle: String, _ action: (() -> Void)? = nil) {
         self.title = title
         self.message = message
         self.buttonTitle = buttonTitle
@@ -22,14 +22,40 @@ struct ErrorInfo {
 }
 
 enum ApiError: Error {
-    case serverError
+    case incorrectId
+    case unableToCreateLobby
+    case unableToDeleteLobby
+    case unableToStartLobby
+    case unableToRestartLobby
+    case unableToGetGenres
+    case unableToConfirmGenres
+    case unableToGetMovies
 }
 
 extension ApiError {
     var errorInfo: ErrorInfo {
         switch self {
-        case .serverError:
-            return ErrorInfo("title", "message", "ok", nil)
+        case .incorrectId: ErrorInfo("Некорректный ID",
+                                     "Введите корректный ID комнаты",
+                                     "Хорошо")
+        case .unableToCreateLobby: ErrorInfo("Не удалось создать лобби",
+                                             "Проверьте интернет",
+                                             "Хорошо")
+        case .unableToDeleteLobby: ErrorInfo("Не удалось удалить лобби",
+                                             "Проверьте интернет",
+                                             "Хорошо")
+        case .unableToStartLobby, .unableToRestartLobby: ErrorInfo("Не удалось начать игру",
+                                                                   "Проверьте интернет",
+                                                                   "Хорошо")
+        case .unableToGetGenres: ErrorInfo("Не удалось получить жанры",
+                                           "Проверьте интернет",
+                                           "Хорошо")
+        case .unableToConfirmGenres: ErrorInfo("Не удалось подтвердить жанры",
+                                               "Проверьте интернет",
+                                               "Хорошо")
+        case .unableToGetMovies: ErrorInfo("Не удалось получить фильмы",
+                                           "Проверьте интернет",
+                                           "Хорошо")
         }
     }
 }
